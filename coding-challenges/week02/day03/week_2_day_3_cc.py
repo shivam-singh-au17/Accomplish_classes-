215fc462177392fc03b764f0e0bfc8cbb72ab931
enter your name <shivamsingh4458@gmail.com>
2021-04-24 17:25:58 +0530

    week02_day03_cc file

---
+++ coding-challenges/week02/day03/week_2_day_3_cc.py
@@ -0,0 +1,20 @@
+# 1. Write python program to ask a user's name, age and telephone number
+# and welcome the user.
+
+user1 = input("Enter your user name: ")
+user2 = input("Enter your age: ")
+user3 = input("Enter your telephone number: ")
+print(user1)
+print(user2)
+print(user3)
+print(user1, "Welcome to the world of python programming.")
+
+# 2. Given 2 numbers as input from the user, find their sum and print it.
+
+num1 = input("Enter 1st number: ")
+num2 = input("Enter 2nd number: ")
+total = (num1 + num2)
+print("Total sum of string is: ",total)
+
+# (If we take the number as input, we will get the answer in the form of a string.)
+
