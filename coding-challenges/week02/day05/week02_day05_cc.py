5aeb7c076762f444ffaa014cac5d9aa268cf0349
enter your name <shivamsingh4458@gmail.com>
2021-04-24 17:27:10 +0530

    week02_day05_cc file

---
+++ coding-challenges/week02/day05/week02_day05_cc.py
@@ -0,0 +1,26 @@
+# a. Take 2 numbers as input from the user and print True if the first number is greater, false otherwise.
+
+
+num1 = int(input("Enter number one: "))
+num2 = int(input("Enter number two: "))
+
+greater = num1 > num2
+print(greater)
+
+
+
+# b. Take 1 number as input from the user. Print its table from 1 to 10.
+
+
+table_num = int(input("Enter number for table: "))
+
+print("2 X 1 = ", table_num * 1)
+print("2 X 2 = ", table_num * 2)
+print("2 X 3 = ", table_num * 3)
+print("2 X 4 = ", table_num * 4)
+print("2 X 5 = ", table_num * 5)
+print("2 X 6 = ", table_num * 6)
+print("2 X 7 = ", table_num * 7)
+print("2 X 8 = ", table_num * 8)
+print("2 X 9 = ", table_num * 9)
+print("2 X 10 = ", table_num * 10)
