e5d1290a3b0a6e6b273237d541d35de91948effb
Shivam Singh <shivamsingh4458@gmail.com>
2021-05-20 00:12:58 +0530

    week07-day02-cc

--- coding-challenges/week07/day02/week07-day02-cc.py
+++ coding-challenges/week07/day02/week07-day02-cc.py
@@ -145,11 +145,3 @@ if __name__ == "__main__":
 
 # ----------------------------------------------------------------
 
-
-
-
-
-
-
-
-# amol amolamolamolamolamolamolamolamolamolamolamolamolamolamolamolamolamolamol
